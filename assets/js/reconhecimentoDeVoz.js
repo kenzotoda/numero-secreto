2aa0cc3becb8b742b35aa184a3b844d46dc7de60
kenzotoda <160362931+kenzotoda@users.noreply.github.com>
2024-06-13 11:50:58 -0300

    Estilos, Número aleatório e Início de voz

---
+++ assets/js/reconhecimentoDeVoz.js
@@ -0,0 +1,6 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
+
